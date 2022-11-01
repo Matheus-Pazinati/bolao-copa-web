2130c93ffd4a6bd39454dcd48086125402764f67
Matheus Pazinati <matheuspazinati@gmail.com>
2022-11-01 03:09:25 -0300

    Busca dados da API no lado servidor e utiliza esses dados na interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
+interface HomeProps {
+  count: number
+}
+
+export default function Home({ count }: HomeProps) {
   return (
-    <h1>Bolão da Copa</h1>
+    <h1>Contagem: {count}</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const request = await fetch('http://localhost:3333/pools/count')
+  const data = await request.json()
+
+  return {
+    props: {
+      count: data.count
+    }
+  }
 }
